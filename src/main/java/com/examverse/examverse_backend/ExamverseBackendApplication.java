decf689513c1439e079d41602f912e583b4ba6f2
trivedi089 <satyachbs007@gmail.com>
2025-11-03 11:26:43 +0530

    initial-setup

---
+++ src/main/java/com/examverse/examverse_backend/ExamverseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.examverse.examverse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamverseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examverse/examverse_backend/ExamverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examverse.examverse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
